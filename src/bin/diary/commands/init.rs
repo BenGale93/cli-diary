22ab5c9c99e64920f678ad2da5072d5ede9f587c
Ben Gale <bengale2007@googlemail.com>
2021-11-03 18:10:24 +0000

    Adjusted library structure.
    Made ops a folder rather than just a file.

--- src/bin/diary/commands/init.rs
+++ src/bin/diary/commands/init.rs
@@ -2,7 +2,8 @@ extern crate clap;
 use std::path::PathBuf;
 
 use clap::{App, Arg, ArgMatches, SubCommand};
-use diary::{ops, CliResult, Config};
+use diary::ops::init;
+use diary::{CliResult, Config};
 
 pub fn cli() -> App<'static, 'static> {
     SubCommand::with_name("init")
@@ -10,15 +11,15 @@ pub fn cli() -> App<'static, 'static> {
         .arg(Arg::with_name("path").default_value("."))
 }
 
-fn args_to_init_ops(args: &ArgMatches<'_>) -> ops::InitOptions {
-    ops::InitOptions {
+fn args_to_init_ops(args: &ArgMatches<'_>) -> init::InitOptions {
+    init::InitOptions {
         path: PathBuf::from(args.value_of("path").unwrap()),
     }
 }
 
 pub fn exec(config: Config, args: &ArgMatches<'_>) -> CliResult {
     let opts = args_to_init_ops(args);
-    let path = ops::init(opts, &config)?;
+    let path = init::init(opts, &config)?;
     let new_cfg = Config::new(path);
     confy::store("diary", new_cfg)?;
     println!("Ran init command");
---
+++ src/diary/ops/mod.rs
@@ -0,0 +1,4 @@
+pub mod init;
+
+pub use self::init::init;
+pub use self::init::InitOptions;
