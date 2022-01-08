0e2f88b7020b692a6d51b57e7e2b86288c25e08d
Ben Gale <bengale2007@googlemail.com>
2022-01-08 21:22:11 +0000

    Integration tests for non-command related aspects.

--- tests/cli.rs
+++ tests/cli.rs
@@ -9,10 +9,33 @@ mod utils;
 fn test_help_text() -> utils::TestReturn {
     let mut cmd = Command::cargo_bin("diary")?;
 
-    cmd.arg("help");
     cmd.assert()
         .success()
         .stdout(predicate::str::contains("Add a new sub-entry"));
+
+    Ok(())
+}
+
+#[test]
+fn test_version_text() -> utils::TestReturn {
+    let mut cmd = Command::cargo_bin("diary")?;
+
+    cmd.arg("--version");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(env!("CARGO_PKG_VERSION")));
+
+    Ok(())
+}
+
+#[test]
+fn test_bad_command() -> utils::TestReturn {
+    let mut cmd = Command::cargo_bin("diary")?;
+
+    cmd.arg("fake");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("wasn't expected"));
 
     Ok(())
 }
